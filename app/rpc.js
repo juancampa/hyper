4f0b3effa354b2eeb3fff4403b7a647770ba4a88
Juan Campa <juancampa@gmail.com>
2018-12-10 14:02:05 -0500

    Demo commit with webgl renderer + improved IPC

--- app/rpc.js
+++ app/rpc.js
@@ -23,6 +23,10 @@ class Server extends EventEmitter {
     this.wc.on('did-finish-load', () => {
       this.wc.send('init', uid);
     });
+
+    this.prefix = this.id;
+    this.batch = Buffer.alloc(1024 * 1024 * 64);
+    this.offset = 0;
   }
 
   get wc() {
@@ -33,8 +37,50 @@ class Server extends EventEmitter {
     super.emit(ev, data);
   }
 
+  flushBatch() {
+    if (this.offset > 0) {
+      const prefix = this.batch.toString('utf8', 0, 36);
+      const block = 1024 * 1024;
+      for (let start = 0; start < this.offset; start += block) {
+        const data = (start > 0 ? prefix : '') +
+          this.batch.toString('utf8', start, Math.min(this.offset, start + block));
+        this.wc.send(this.id, {
+          ch: this.ch,
+          data,
+        });
+      }
+      // this.wc.send(this.id, {
+      //   ch: this.ch,
+      //   data: this.batch.toString('utf8', 0, this.offset),
+      // });
+      this.offset = 0;
+      this.batchSize = 0;
+      this.batchStart = 0;
+    }
+  }
+
+
   emit(ch, data) {
-    this.wc.send(this.id, {ch, data});
+    // this.wc.send(this.id, {ch, data });
+    // return;
+    if (typeof data !== 'string') {
+      this.flushBatch();
+      this.wc.send(this.id, {ch, data});
+    } else {
+      if (this.offset === 0) {
+        this.ch = ch;
+        setTimeout(() => this.flushBatch(), 16);
+
+        this.batchStart = Date.now();
+        this.batch.write(data, 0);
+        this.offset = data.length;
+      } else {
+        const chunk = data.slice(36);
+        this.batch.write(chunk, this.offset);
+        this.offset += chunk.length;
+      }
+      this.batchSize += 1;
+    }
   }
 
   destroy() {
